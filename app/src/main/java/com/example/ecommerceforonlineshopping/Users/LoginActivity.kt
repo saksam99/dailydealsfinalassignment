eb8f2bc2b9a23c017a72637eeeca40ee27050e6a
Saksham Shrestha <180180@softwarica.edu.np>
2021-01-22 23:41:24 +0545

    Dailydealsfinalassignment

---
+++ app/src/main/java/com/example/ecommerceforonlineshopping/Users/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ecommerceforonlineshopping.Users
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.ecommerceforonlineshopping.MainActivity
+import com.example.ecommerceforonlineshopping.R
+ 
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/ecommerceforonlineshopping/Users/RegistrationActivity.kt
@@ -0,0 +1,27 @@
+package com.example.ecommerceforonlineshopping.Users
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import com.example.ecommerceforonlineshopping.R
+
+class RegistrationActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+    }
+
+
+}
+
+
